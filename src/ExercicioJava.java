58dbbadc668abc227c5f29a98c35ee75fdd8f98a
Ícaro <icaroanthony1@gmail.com>
2022-09-08 11:12:36 -0300

    aula

---
+++ src/ExercicioJava.java
@@ -0,0 +1,24 @@
+
+    /* - Grupo:
+    - George Marques
+    - Bruno Giuseppe
+    - Cleber Rockemback
+    - Marcelo Victor
+    - Gustavo Azevedo
+ */
+
+    public class ExercicioJava {
+        public static void main(String[] args) {
+            int i = 1;
+            while(i<=20){
+                System.out.print(i);
+                i++;
+                if(i%5 == 0){
+                    System.out.println();
+                } else{
+                    System.out.print('\t');
+                }
+            }
+        }
+    }
+
